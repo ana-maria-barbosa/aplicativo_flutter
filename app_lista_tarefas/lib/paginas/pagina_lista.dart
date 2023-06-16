309123b829357396aaaefe04715d781c962d7fc7
ana <anamaria347200@gmail.com>
2023-06-16 11:58:23 -0300

    codigos

---
+++ app_lista_tarefas/lib/paginas/pagina_lista.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+
+class  Pagina_Lista extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(child: TextField(),)
+    );
+  }
+  }
