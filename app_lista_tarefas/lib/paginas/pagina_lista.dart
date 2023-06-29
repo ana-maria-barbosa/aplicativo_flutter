235ede2470bae7888ac572c0eb529d8957a6fea0
ana <anamaria347200@gmail.com>
2023-06-29 10:24:01 -0300

    enviando codigo

--- app_lista_tarefas/lib/paginas/pagina_lista.dart
+++ app_lista_tarefas/lib/paginas/pagina_lista.dart
@@ -1,10 +1,37 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
+final TextEditingController email_controle = TextEditingController();
 
 class  Pagina_Lista extends StatelessWidget{
   Widget build(BuildContext context){
     return Scaffold(
-      body: Center(child: TextField(),)
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+            TextField(
+            controller: email_controle,
+            decoration: InputDecoration(
+              labelText: 'Email',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: entrar, 
+            child:Text ("Entrar")
+            ),
+          ],),
+        ),
+      ),
     );
   }
+  }
+
+  void entrar (){
+    String texto = email_controle.text;
+    print(texto);
+    email_controle.clear();
   }
